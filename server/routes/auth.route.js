06096cf26570bdf2b0a0929614c94fea5c59b0d7
Jorden Tang <jordentangwork@gmail.com>
2020-02-18 14:07:40 -0800

    whatever

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -5,20 +5,18 @@ module.exports = (app) =>{
         // create a user a new user
         User.findOne({email: req.body.email}, function(err, user){
             if(err) throw err
-            console.log(user)
-
+            // console.log(user)
             //when admin logged in
-            if(user.isAdmin){
-                console.log("admin has logged in")
-            }
-
             user.comparePassword(req.body.password, function(err, isMatch){
                 if(err) throw err;
                 //Temporary response and error handling for password matching
                 if(!isMatch){
-                    throw new Error("Broken")
+                    res.json({err: "password didn't match"})
                 }
                 else{
+                    if(user.isAdmin){
+                        console.log("admin has logged in")
+                    }
                     res.json({good : "good"})
                 }
             })
