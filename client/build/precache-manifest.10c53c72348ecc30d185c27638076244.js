2fce4df92ae7491bddf002dfe407d01db04e8146
Jorden Tang <jordentangwork@gmail.com>
2020-02-21 18:58:54 -0800

    1.fixed price sorting bug 2. issue: multiple updates on the same scroll won't work

---
+++ client/build/precache-manifest.10c53c72348ecc30d185c27638076244.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d946430a2dee6482847281b409709324",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40f7000bc2b82e9f3c58",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ebb6742c6cf18b88e509",
+    "url": "/static/css/main.af9a54f7.chunk.css"
+  },
+  {
+    "revision": "40f7000bc2b82e9f3c58",
+    "url": "/static/js/2.859f9d0e.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.859f9d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb6742c6cf18b88e509",
+    "url": "/static/js/main.c829dc8d.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "2717ed7074ad3ea24bfed2d155761682",
+    "url": "/static/media/Mushroom.2717ed70.png"
+  },
+  {
+    "revision": "b29de028edffc86119288d3ed9259f2c",
+    "url": "/static/media/Slime.b29de028.png"
+  },
+  {
+    "revision": "d31eaa7ab3148597bbdd76e52ec8ee75",
+    "url": "/static/media/background.d31eaa7a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
