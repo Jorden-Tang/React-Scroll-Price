e09a862471b769f92fc703dca81c521d0d4a74f4
JordenTang <Jordentangwork@gmail.com>
2020-05-19 01:12:28 -0700

    added boss event model 1

--- server/models/event.model.js
+++ server/models/event.model.js
@@ -3,8 +3,9 @@ const user = require('./User.model')
 const eventSchema = new mongoose.Schema({
     eventTitle: {type: String, required: [true, 'need to specify boss event type'], lowercase: true},
     host: {type: user, required: [true, 'need host for boss party']},
-    buyer: [user],  
+    buyer: [user],
+    
 }, {timestamps: true})
 
 const Event = mongoose.model("Event", eventSchema)
-module.exports = Event;
+module.exports = Event
