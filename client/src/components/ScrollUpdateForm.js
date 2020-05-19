6cb9f449193a50814d4b3f1ecf88155b1fc83a59
JordenTang <Jordentangwork@gmail.com>
2020-05-18 19:02:21 -0700

    fixed scroll reference/clone bug

--- client/src/components/ScrollUpdateForm.js
+++ client/src/components/ScrollUpdateForm.js
@@ -35,7 +35,8 @@ const ScrollUpdateForm = (props) =>{
     const copyEntry = (e, i)=>{
         e.preventDefault();
         console.log(data.scrolls[i])
-        data.scrolls.splice(i, 0, data.scrolls[i]);
+        const scrollClone ={...data.scrolls[i]};
+        data.scrolls.splice(i+1, 0, scrollClone);
         setData({ scrolls: data.scrolls });
  
     }
